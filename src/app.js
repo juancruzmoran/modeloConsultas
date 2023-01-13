6fcc1701b2de1b86b5f275f38097e4af6608e6e3
juancruzmoran <juancruzmoran@outlook.es>
2023-01-12 21:04:57 -0300

    controllers/routes/views

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 
+
 const indexRouter = require('./routes/index');
 
 const moviesRoutes = require('./routes/moviesRoutes');
@@ -13,8 +14,12 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
+//URL encode  - Para que nos pueda llegar la información desde el formulario al req.body
+app.use(express.urlencoded({ extended: false }));
+
+
 app.use('/', indexRouter);
-app.use(moviesRoutes);
+app.use('/movies',moviesRoutes);
 app.use(genresRoutes);
 
 app.listen('3001', () => console.log('Servidor corriendo en el puerto 3001'));
